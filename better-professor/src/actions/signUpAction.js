51187903ebd6c7dd44a3df35f72009fc45f8309a
Maria Trujillo <mptruj015@gmail.com>
2020-04-28 17:33:02 +0200

    updated post request

--- better-professor/src/actions/signUpAction.js
+++ better-professor/src/actions/signUpAction.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const postSignUp = (newUser) => dispatch =>{
     dispatch({type: 'SIGNUP_START'})
     axios
-    .put('https://better-professor-karavil.herokuapp.com/auth/register', newUser)
+    .post('https://better-professor-karavil.herokuapp.com/auth/register', newUser)
     .then(res => {
         console.log(res)
         dispatch({type: 'SIGNUP_SUCCESS', payload: res})
