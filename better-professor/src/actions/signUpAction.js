43a292ef71bd40632fae3a07319d518a08bb5034
Maria Trujillo <mptruj015@gmail.com>
2020-04-28 19:04:22 +0200

    worked on post req bug

--- better-professor/src/actions/signUpAction.js
+++ better-professor/src/actions/signUpAction.js
@@ -2,8 +2,16 @@ import axios from 'axios'
 
 export const postSignUp = (newUser) => dispatch =>{
     dispatch({type: 'SIGNUP_START'})
+
+    const formattedUser = {
+       ...newUser,
+       first_name: newUser.firstName,
+       last_name: newUser.lastName
+    }
+
+    console.log(newUser)
     axios
-    .post('https://better-professor-karavil.herokuapp.com/auth/register', newUser)
+    .post('https://better-professor-karavil.herokuapp.com/auth/register', formattedUser)
     .then(res => {
         console.log(res)
         dispatch({type: 'SIGNUP_SUCCESS', payload: res})
