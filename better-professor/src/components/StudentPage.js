6ac1dcb04f3aa5d3817ddd44cd3feeb1461af1bd
Maria Trujillo <mptruj015@gmail.com>
2020-04-30 01:11:06 +0200

    get students and add students functionality is working

--- better-professor/src/components/StudentPage.js
+++ better-professor/src/components/StudentPage.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import StudentList from './StudentList'
+import AddStudent from './AddStudent'
 
 const StudentPage = () => {
+
+    const [studentList, setStudentList] = useState([])
+
+    const getStudents = () => {
+        axiosWithAuth()
+        .get("https://better-professor-karavil.herokuapp.com/students")
+        .then( res => {
+            console.log("from get students", res.data.students)
+            setStudentList(res.data.students)
+        })
+        .catch(err => {
+            console.log("couldn't get the students", err)
+        })
+    }
+
+    // setStudentList(studentList => {...studentList, newStudent});
+    useEffect(() => {
+        getStudents()
+    }, [])
+
     return (
-        <div>I am a protected page</div>
+     <>
+     <h1>I am rendered</h1>
+        Student List:
+        <StudentList students={studentList} updateStudents={setStudentList}/>
+        Add Student:
+        <AddStudent setStudentList={setStudentList}/>
+     </>
     )
 }
 export default StudentPage;
