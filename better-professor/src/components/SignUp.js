e439b4ef3050dde1d4dbd4b2b50af65b472d93df
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-04-30 09:18:15 -0400

    added backdround/other styles and yup validation to sign up page

--- better-professor/src/components/SignUp.js
+++ better-professor/src/components/SignUp.js
@@ -1,11 +1,48 @@
- import React, { useState } from 'react'
+ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import * as yup from 'yup';
 import { postSignUp } from '../actions/signUpAction'
-import { Signup, SignupForm } from './styledComponents/signupContainer';
+import { Signup, SignupForm, Div } from './styledComponents/signupContainer';
+import bg from './background.png';
 
 
+
+const initFormValue = {
+    firstName:'',
+    lastName:'',
+    email:'',
+    password:'',
+
+
+}
+const initFormErrors = {
+    firstName:'',
+    lastName:'',
+    email:'',
+    password:'',
+
+}
+const formSchema = yup.object().shape({
+    firstName: yup
+    .string()
+    .required('First name field is required')
+    .min(2),
+    lastName: yup
+    .string()
+    .required('Last name field is required')
+    .min(2),
+    email: yup
+    .string()
+    .email('Enter a valid email')
+    .required('Email field is required'),
+    password: yup
+    .string()
+    .required('Password field is required')
+})
 const SignUp = props => {
-
+    const [formValue, setFormValue] = useState(initFormValue)
+    const [formErrors, setFormErrors] = useState(initFormErrors)
+    const [isDisabled, setIsDisabled] = useState(true)
     const [signUp, setSignUp] = useState({
         firstName: "",
         lastName: "",
@@ -13,16 +50,43 @@ const SignUp = props => {
         password: ""
     });
 
+    useEffect(()=>{
+        formSchema.isValid(formValue)
+         .then(valid=>{
+             setIsDisabled(!valid)
+         })
+    },[formValue])
+
     const handleChanges = e => {
-        setSignUp({...signUp, [e.target.name]: e.target.value});
+        const name = e.target.name;
+        const value = e.target.value;
+
+        yup.reach(formSchema, name).validate(value)
+        .then(valid=>{
+            setFormErrors({
+                ...formErrors,
+                [name]:''
+            })
+        })
+        .catch(err=>{
+            setFormErrors({
+                ...formErrors,
+                [name]:err.errors[0]
+            })
+        })
+
+        setFormValue({
+            ...formValue, 
+            [name]:value 
+        });
     }
 
     const handleSubmit = e => {
         e.preventDefault()
-        console.log(signUp)
-        props.postSignUp(signUp);
+        console.log(formValue)
+        props.postSignUp(formValue);
 
-        setSignUp({
+        setFormValue({
             firstName: "",
             lastName: "",
             email: "",
@@ -31,66 +95,81 @@ const SignUp = props => {
     }
 
     return (
+        <Div style={{backgroundImage:`url(${bg})`}}>
+    <Signup>
+
+        <h1 style={{textAlign:'center'}}>Welcome to Better Professor</h1>
+        <p style={{marginTop:'0px',textAlign:'center'}}>A better way to mentor</p>
+        <h3 style={{textAlign:'center'}}>Sign Up</h3>
+        <p style={{ color:'red', marginTop:'0px',textAlign:'center'}}>{formErrors.firstName}</p>
+        <p style={{ color:'red', marginTop:'0px',textAlign:'center'}}>{formErrors.lastName}</p>
+        <p style={{ color:'red', marginTop:'0px',textAlign:'center'}}>{formErrors.email}</p>
+        <p style={{ color:'red', marginTop:'0px',textAlign:'center'}}>{formErrors.password}</p>
+        <SignupForm onSubmit={handleSubmit}>
             <div>
-            <h1>Welcome to Better Professor</h1>
-            <p>A better way to mentor</p>
-        <Signup>
+            <label htmlFor="first-name">
+                First Name:
+                <input
+                    type="text"
+                    name="firstName"
+                    value={formValue.firstName}
+                    onChange={handleChanges}
+                />
+            </label>
 
-            <h3>Sign Up</h3>
-            <SignupForm onSubmit={handleSubmit}>
-                <label htmlFor="first-name">
-                    First Name:
-                    <input
-                        type="text"
-                        name="firstName"
-                        value={signUp.firstName}
-                        onChange={handleChanges}
-                    />
-                </label>
+            <label htmlFor="Last Name">
+                Last Name:
+                <input
+                    style={{
+                        marginTop:'3%'
+                    }}
+                    type="text"
+                    name="lastName"
+                    value={formValue.lastName}
+                    onChange={handleChanges}
+                />
+            </label>
+            </div>
 
-                <label htmlFor="Last Name">
-                    Last Name:
-                    <input
-                        style={{
-                            marginTop:'3%'
-                        }}
-                        type="text"
-                        name="lastName"
-                        value={signUp.lastName}
-                        onChange={handleChanges}
-                    />
-                </label>
+            <label htmlFor="email">
+                Email:
+                <input
+                    style={{
+                        marginTop:'3%'
+                    }}
+                    type="email"
+                    name="email"
+                    value={formValue.email}
+                    onChange={handleChanges}
+                />
+            </label>
 
-                <label htmlFor="email">
-                    Email:
-                    <input
-                        style={{
-                            marginTop:'3%'
-                        }}
-                        type="email"
-                        name="email"
-                        value={signUp.email}
-                        onChange={handleChanges}
-                    />
-                </label>
+            <label>
+                Password:
+                <input
+                    style={{
+                        marginTop:'3%'
+                    }}
+                    type="password"
+                    name="password"
+                    value={formValue.password}
+                    onChange={handleChanges}
+                />
+            </label>
 
-                <label>
-                    Password:
-                    <input
-                        style={{
-                            marginTop:'3%'
-                        }}
-                        type="password"
-                        name="password"
-                        value={signUp.password}
-                        onChange={handleChanges}
-                    />
-                </label>
+            <button disabled={isDisabled} onClick={handleSubmit} style={{width:'25%', margin:'5% auto'}}>Sign Up</button>
+        <img alt='icon pointing to input fields' src={require('./teacher.png')} style={{
+            width:'25%', 
+            transform: 'scaleX(-1)',
+            position:'absolute',
+            right:'10%',
+            bottom:'-40%',
 
-                <button style={{width:'25%', margin:'5% auto'}}>Sign Up</button>
-            </SignupForm>
-        </Signup>
-        </div>
+            
+            }}/>
+        </SignupForm>
+    </Signup>
+    </Div>
 
     )
 }
--- better-professor/src/components/styledComponents/signupContainer.js
+++ better-professor/src/components/styledComponents/signupContainer.js
@@ -1,14 +1,33 @@
 import styled from 'styled-components';
 
 export const Signup = styled.div`
-    width:25%;
+    width:45%;
     border:1px solid black;
-    height:35vh;
+    display:flex;
+    flex-direction:column;
+    text-align:left;
+    padding:2%;
+    /* height:35vh; */
     margin:0 auto;
     border-radius:6%;
+    box-shadow: 2px 2px 5px 4px #000;
+    /* margin-top:10%; */
 
+    background-color:white;
 `;
 export const SignupForm = styled.form`
     display:flex;
+    position:relative;
     flex-direction:column;
+    input[type=text],input[type=email],input[type=password] {
+    border: none;
+    border-bottom: 1px solid darkgray;
+    width:30%;
+    }
 `;
+export const Div = styled.div`
+    height:94vh;
+    padding-top:8%
+    
+`;
+
